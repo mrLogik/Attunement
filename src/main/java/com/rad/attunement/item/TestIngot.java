f98304c5d46f4a57b08f7545fdd62fd2a2d3e6b7
mrLogik <jonathon.whittington@gmail.com>
2014-08-01 17:05:05 -0500

    Added missing java file for "TestIngot.java".

---
+++ src/main/java/com/rad/attunement/item/TestIngot.java
@@ -0,0 +1,10 @@
+package com.rad.attunement.item;
+
+public class TestIngot extends AttuneItem {
+	
+    public TestIngot() {
+        super();
+        this.setUnlocalizedName("testIngot");
+    }
+    
+}

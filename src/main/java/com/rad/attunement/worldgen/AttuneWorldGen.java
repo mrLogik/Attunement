73bcc72ab5cc1ac4078aec782b95df0645741c59
Zywicki <andrew_z@sbcglobal.net>
2014-08-03 15:30:08 -0500

    Ore Generation
    Added ore gen for TestOre

--- src/main/java/com/rad/attunement/worldgen/AttuneWorldGen.java
+++ src/main/java/com/rad/attunement/worldgen/AttuneWorldGen.java
@@ -4,10 +4,12 @@ import java.util.Random;
 
 import com.rad.attunement.block.*;
 
+import com.rad.attunement.init.ModBlocks;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
 import cpw.mods.fml.common.IWorldGenerator;
+import net.minecraft.world.gen.feature.WorldGenMinable;
 
 public class AttuneWorldGen implements IWorldGenerator {
 	
@@ -15,5 +17,32 @@ public class AttuneWorldGen implements IWorldGenerator {
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
 		// params: Block X, Block Y, Block Z, Block;
 		world.setBlock(0, 100, 1, Blocks.brick_stairs );
+
+        switch(world.provider.dimensionId){
+            case -1:
+                generateNether(world, random, chunkX * 16, chunkZ * 16);
+                break;
+            case 0:
+                generateSurface(world, random, chunkX * 16, chunkZ * 16);
+                break;
+            case 1:
+                generateEnd(world, random, chunkX * 16, chunkZ * 16);
+                break;
+        }
     }
+
+    private void generateEnd(World world, Random rand, int chunkX, int chunkZ) {}
+
+    private void generateSurface(World world, Random rand, int chunkX, int chunkZ) {
+        for(int k = 0; k < 10; k++){
+            int xPos = chunkX + rand.nextInt(16);
+            int yPos = rand.nextInt(64);
+            int zPos = chunkZ + rand.nextInt(16);
+
+            // params(target block(for our mod it has to reference "ModBlocks.ExampleBlock"), ore vein size(number of blocks per vein))
+            (new WorldGenMinable(ModBlocks.testOre, 13)).generate(world, rand, xPos, yPos, zPos);
+        }
+    }
+
+    private void generateNether(World world, Random rand, int chunkX, int chunkZ) {}
 }
